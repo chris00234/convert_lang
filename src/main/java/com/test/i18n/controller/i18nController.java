2cf947edaf755fb7afd29b9092d0ec7384c64ddd
wheogus98@gmail.com <daehyunc@uci.edu>
2023-02-26 02:03:00 -0800

    commit

--- src/main/java/com/test/i18n/controller/i18nController.java
+++ src/main/java/com/test/i18n/controller/i18nController.java
@@ -1,5 +1,13 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class i18nController {
 
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
 }
